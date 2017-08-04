a1d04de9de4ca17ad130e5ea3d5122b62bd52aa7
Raffaele Calzà <raffaelecalza4@gmail.com>
2017-08-05 01:04:57 +0200

    Fixed notifications' JSON path

--- bot/notification-manager.js
+++ bot/notification-manager.js
@@ -15,15 +15,15 @@ const fs = require('fs');
 var exports = module.exports = {};
 
 exports.fileExists = () => {
-    return fs.existsSync('user-notification.json');
+    return fs.existsSync(__dirname + '/user-notification.json');
 }
 
 exports.loadFile = () => {
-    return JSON.parse(fs.readFileSync('user-notification.json', 'utf8'));
+    return JSON.parse(fs.readFileSync(__dirname + '/user-notification.json', 'utf8'));
 }
 
 exports.saveFile = (obj) => {
-    fs.writeFile('user-notification.json', JSON.stringify(obj), (err) => {
+    fs.writeFile(__dirname + '/user-notification.json', JSON.stringify(obj), (err) => {
         if(err) throw err;
     });
 }
